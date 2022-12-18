29682c7f76189d25873cd4f485154e1fc7b4093a
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-12-18 16:29:46 +0530

    coding maktab

---
+++ Basic/OperatorExample.py
@@ -0,0 +1,30 @@
+#Basic operator in python
+# Arithmetic Operator (+,-,*,/,%)
+x=100
+y=200
+#sum of x and y in cal variable
+cal=x+y #sum operation
+print(cal)
+#substraction
+n=500
+m=600
+cal=m-n #substraction operation
+print(cal)
+#multiplication
+h=20
+i=30
+cal=h*i #multipliation
+print(cal)
+#division
+k=400
+l=20
+cal=k / l #division
+print(cal)
+
+#modulo
+c=20
+d=7
+cal= c%d #mudulo oprations
+print(cal)
+
+
---
+++ Basic/VariableExample.py
@@ -0,0 +1,43 @@
+# - one line comment in python
+# comment -> Variable - variable is place where we store/assign the value in your system.
+# 100, 200, 100+200 => 300
+100 + 200
+#print() -> this is the method which we used to show the result at console
+print(100+200)
+# multiply with 3 into above result
+cal=100+200
+print(cal)
+# multiply with 3
+print(cal*3)
+
+# result may be inform of your calculated value or string message
+# display message and calculated value
+print("Sum is -> ",cal)
+# sum of 100 and 200 is 300
+a=100
+b=200
+print('Sum of', {a}, 'and', {b},' is ',cal)
+print(f'Sum of {a} and {b} is ->',cal)
+print(f'Multiple of {a} and {b} is ->',a*b)
+
+#Does for variable
+#num,x,y,z
+#employee_sal, house_rent
+#num1, num2
+#Don't for variable
+#1num
+#employee-sal, house-rent
+#$salary,^rent
+#reserved keyword of python
+
+x=100
+print(x)
+x=100.5
+print(x)
+x='coding maktab'
+print(x)
+
+x=100
+x=200.5
+x='Welcome coding maktab'
+print(x)
