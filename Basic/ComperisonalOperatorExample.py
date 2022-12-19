3a89f296050f435eae6bb0d4782a5f4968b0c8a4
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-12-19 17:50:04 +0530

    coding maktab

---
+++ Basic/ComperisonalOperatorExample.py
@@ -0,0 +1,10 @@
+#Operator => >,<,>=,<=, ==
+# These above operator not only return True or False
+# The use of this operator is to compare value (number,string,date any thing)
+res= 100 > 200 # left value compares the right value
+print(res) #False
+res=100<200
+print(res) # True (left value is less to right value)
+
+res=100 == 200
+print(res) # False left value is not equal to right value
