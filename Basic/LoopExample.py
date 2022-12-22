55bf9bfc4019f5e22384f81cc61a4cf5b10cde19
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-12-22 09:46:24 +0530

    coding maktab

---
+++ Basic/LoopExample.py
@@ -0,0 +1,30 @@
+#Usage of loop
+# Hello world for ten times
+print('Hello! World')
+print('Hello! World')
+print('Hello! World')
+print('Hello! World')
+print('Hello! World')
+print('Hello! World')
+print('Hello! World')
+print('Hello! World')
+print('Hello! World')
+print('Hello! World')
+
+# print Hello! World 1000 times
+#language provide easiest method to iterate your work, which is feasible
+# loop (while) - iteration
+#construct
+#Step -1
+#intialize a variable
+# Step -2
+# use the intialize in a condition -> condition is bind up with while keyword
+# Step -3
+# Write your process
+# Step -4
+# increase or decrease the variable (so that at the point of time the condition must fail to complete the process).
+#print 10 times Hello! World using loop
+x=1
+while x<=10:
+    print('Hello! World')
+    x=x+1
