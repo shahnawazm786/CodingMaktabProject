bd9580ce73cd6743832c00a49e2d3e1588f7bceb
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-12-24 15:36:04 +0530

    coding maktab

---
+++ Basic/ForLoopExample.py
@@ -0,0 +1,11 @@
+# for loop is used on object
+# write the program to print 1 to 10 using for loop
+for x in range(11):
+    print(x)
+print('1 to 10')
+for x in range(1,11):
+    print(x)
+
+name="my name is mohammad"
+for n in name:
+    print(n)
