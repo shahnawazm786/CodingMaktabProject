97e1579b720fa14362965fb727e6516d94fbe6f6
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-12-24 22:40:57 +0530

    coding maktab

--- Turing/Turing4_api.py
+++ Turing/Turing4_api.py
@@ -1,6 +1,13 @@
+import json
+
 import requests
 import jsonpath
 
 url="https://reqres.in/api/users?page=2"
 res=requests.get(url)
 print(res.content)
+
+#parse response to json path
+
+json_response=json.loads(res.text)
+print(json_response)
