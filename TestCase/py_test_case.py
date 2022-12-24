75b29966dfaa1c56040af7a1a9d2129b23aebdd7
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-12-24 23:25:23 +0530

    coding maktab

---
+++ TestCase/py_test_case.py
@@ -0,0 +1,40 @@
+import json
+
+import requests
+
+url="https://reqres.in/api/users"
+
+def create_user_using_api():
+    # Read input from json file
+    file = open("C:\\json_files\\employee.json", "r")
+    json_input = file.read()
+    print(json_input)
+    requests_json = json.loads(json_input)
+    print(requests_json)
+
+    # hit the post api url
+    resp = requests.post(url, requests_json)
+    print(resp.status_code)
+
+    assert resp.status_code == 201
+
+    print(resp.content)
+
+    # print form response header
+    print(resp.headers)
+
+    # Content type
+    print(resp.headers.get('Content-Type'))
+
+    # Content length
+    print(resp.headers.get('Content-Length'))
+
+    # Connection
+
+    print(resp.headers.get('Connection'))
+
+    # NEL
+    print(resp.headers.get('NEL'))
+
+    # X-Powered-By
+    print(resp.headers.get('X-Powered-By'))
