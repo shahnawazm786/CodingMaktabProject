71098fa682fb7cccf12726da85e09cbfb6d0d06d
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-12-24 22:50:06 +0530

    coding maktab

--- Turing/Turing_Delete_User_api.py
+++ Turing/Turing_Delete_User_api.py
@@ -9,7 +9,7 @@ url="https://reqres.in/api/users/2"
 
 resp=requests.get(url)
 
-#json_response=json.load(resp.text)
+# fetch the response code
 
-#json_path = jsonpath.jsonpath(json_response,"total_pages")
+print(resp.status_code)
 
