1ce9fdc9af628bb0ca4e1bd8c05375d8b6486bc8
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-12-24 15:03:25 +0530

    coding maktab

---
+++ Turing/ForLoopExample.py
@@ -0,0 +1,21 @@
+x=0
+while x<10:
+    x += 1
+    if x<=4:
+        continue
+    print(x)
+
+
+y=1
+while y<=10:
+    if(y>4):
+        break
+    print(y)
+    y+=1
+print("Z")
+z=1
+while z<=10:
+    if z>=5:
+        pass
+    print(z)
+    z+=1
