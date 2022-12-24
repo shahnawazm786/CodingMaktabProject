1b404ef111b8b46e120c398f5f3b8237361ff5c0
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-12-24 22:38:17 +0530

    coding maktab

--- Turing/Turing3.py
+++ Turing/Turing3.py
@@ -6,4 +6,9 @@ import requests
 url="https://reqres.in/api/users?page=2"
 res=requests.get(url)
 print(res)
+#print headers
+print(res.headers)
+#print response body or content
+print(res.content)
+assert res.status_code == 200
 
