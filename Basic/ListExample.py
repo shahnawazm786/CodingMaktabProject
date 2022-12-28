015bf1233e9552569b48def44bf37f9c4c6341e9
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-12-28 09:17:16 +0530

    coding maktab

--- Basic/ListExample.py
+++ Basic/ListExample.py
@@ -1,7 +1,12 @@
 # List ->
 # Usage of List
 # what is the age of user A
-A=45
+A=45 # single unit and single value
 # Store and process the age of two person
 Person1=60
 Person2=65
+# Person of age the
+# Collection give me the feature to store and process the multiple value in a single unit
+Age=[16,45,35,90,40]
+print(Age)
+
