99a93e4ea47dd5393c12b3478af4b161d9ea1762
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-12-29 08:32:49 +0530

    coding maktab

---
+++ Basic/ListExample1.py
@@ -0,0 +1,8 @@
+# List can have any data into it
+L=['codingMaktab',5000,True,10J]
+print(L)
+L1=[10,20,['codingMaktab','KAZ'],True,100.789,10J]
+print(L1)
+print(type(L1))
+print(type(L))
+print(type(L1[2]))
