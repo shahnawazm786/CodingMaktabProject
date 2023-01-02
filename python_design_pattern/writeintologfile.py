a197a96a58542b6c0f68314af15ce5e1f4150781
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-02 22:40:16 +0530

    coding maktab

--- python_design_pattern/writeintologfile.py
+++ python_design_pattern/writeintologfile.py
@@ -2,6 +2,7 @@ def log_message(msg):
     with open("logger.log","a") as log_file:
         log_file.write("{0}\n".format(msg))
 
+
 log_message("Second line")
 log_message("Third line")
 log_message("Fourth line")
