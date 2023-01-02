dcd39bd7da9238e33a81776883a047e7ac4fd042
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-02 23:23:44 +0530

    coding maktab

--- python_design_pattern/logger_class.py
+++ python_design_pattern/logger_class.py
@@ -4,3 +4,12 @@ class Logger(object):
     Attributes:
         file_name: a string representing the full path of the log file to which this logger will write its messages.
     """
+    def __int__(self,file_name):
+        """ Return a Logger object whose file_name is *file_name*"""
+        self.file_name=file_name
+    def _write_log(self,level,msg):
+        """Write a message to the file_name for a specific Logger instance """
+        with open(self.file_name,"a") as log_file
+            log_file.write("[{0}] {1}\n".format(level,msg))
+    def critical(self,level,msg):
+        self._write_log("CRITICAL",msg)
