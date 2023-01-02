9dd60bef22cbc91884d32f61241a4476b4c85145
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-02 22:47:52 +0530

    coding maktab

--- python_design_pattern/logger.py
+++ python_design_pattern/logger.py
@@ -9,3 +9,7 @@ Debug
 def log_message(msg):
     with open("logger.log","a") as log_file:
         log_file.write("{0}\n".format(msg))
+
+def critical(msg):
+    with open("logger.log","a") as log_file:
+        log_file.write('[Critical] {0}\n'.format(msg))
