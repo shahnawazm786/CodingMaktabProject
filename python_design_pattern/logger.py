248350cefea68e917068f843453ddb48cd75b745
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-02 22:44:58 +0530

    coding maktab

--- python_design_pattern/logger.py
+++ python_design_pattern/logger.py
@@ -1,3 +1,11 @@
+'''
+For the sake of our logger, we will handle  the following levels:
+Critical
+Error
+Warning
+Info
+Debug
+'''
 def log_message(msg):
     with open("logger.log","a") as log_file:
         log_file.write("{0}\n".format(msg))
