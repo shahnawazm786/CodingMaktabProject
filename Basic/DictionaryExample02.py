562330cc1e884f662175aaa2387771cd3b67f76e
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-10 20:57:14 +0530

    coding maktab

--- Basic/DictionaryExample02.py
+++ Basic/DictionaryExample02.py
@@ -8,4 +8,9 @@ a={101:"codingmaktab",102:"kaz-online",101:"Python Program"}
 print(a)
 c={101:"codingmaktab",102:"kaz-online",103:"codingmaktab"}
 print(c)
+#key will always the generic
+d={(10,20):"Circle",2:"Square"}
+print(d)
+#e={[1,2,3]:"Yellow"}
+#print(e)
 
