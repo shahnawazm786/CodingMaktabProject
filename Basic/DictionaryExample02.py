3c4472bc03a11cebd1dc97e1bec121ae97cfa4b8
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-10 20:55:10 +0530

    coding maktab

--- Basic/DictionaryExample02.py
+++ Basic/DictionaryExample02.py
@@ -2,4 +2,10 @@ a={101:"codingmaktab",102:"kaz-online"}
 print(a)
 print(a[101])
 print(a[102])
+# dictionary key is unique
+# dictionary value can be duplicate,
+a={101:"codingmaktab",102:"kaz-online",101:"Python Program"}
+print(a)
+c={101:"codingmaktab",102:"kaz-online",103:"codingmaktab"}
+print(c)
 
