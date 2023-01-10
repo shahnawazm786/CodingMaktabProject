1238a540990267eb0b6427e422ddebbcdff76901
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-10 20:47:13 +0530

    coding maktab

--- Basic/DictionaryExample.py
+++ Basic/DictionaryExample.py
@@ -2,3 +2,6 @@
 # pair entry data
 # {}
 # take parameters for key and value
+# To define dictionary follow the following convention
+a={101:"codingmaktab"}
+# first entry in the barket denote key, and after color denote value
