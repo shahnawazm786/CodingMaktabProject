3cec6df2dfc014545d538f2b2c6c0f60c562b07c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-13 23:30:12 +0530

    coding maktab

---
+++ Training/PrintMethod.py
@@ -0,0 +1,9 @@
+print("Hello!! World")
+# Hello!!! World  - 2023
+s="Hello!!! World"
+year=2023
+print(s+" " +str(year))
+print(s,year)
+x=100
+print("This is my {x} first")
+print("this is python programming",sep=',')
