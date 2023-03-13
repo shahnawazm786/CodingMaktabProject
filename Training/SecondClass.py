b20aca17229c1abff4c08197e24987789ae0573c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-13 23:09:26 +0530

    coding maktab

---
+++ Training/SecondClass.py
@@ -0,0 +1,27 @@
+x=100 # int class
+y=7
+print(x/y)
+print(x//y)
+print(x%y)
+print(type(x))
+x="Hello World" # str class
+print(x)
+print(type(x))
+sal=y*100
+print(sal)
+# conditional  operator (True or False)
+print(100>200)
+print(100<200)
+print(100==100)
+print(100==200)
+print(200>=100)
+print(100<=200)
+print(100<=100)
+print(100!=100)
+print(50!=100)
+print(100!=20)
+print('a'>'b')
+print("Hello"<"World")
+print("Hello"<"hello")
+
+
