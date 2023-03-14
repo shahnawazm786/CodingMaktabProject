59811efefb7b9bd6a7e7f4a9ccf848147a508fc4
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-14 22:13:37 +0530

    coding maktab

---
+++ Training/ConditionalConstructExample.py
@@ -0,0 +1,22 @@
+print('hello')
+x=100
+y=200
+print(x+y)
+print(x-y)
+print(x*y)
+# calcuate the commission of an employee who are engineers
+# abc is 500 employees are working
+# 50 employees are engineers
+# job_title
+job_title='doctors'
+flag=job_title=='engineers'
+print(flag)
+sal=12000
+if flag==True:
+    commission=sal*.05
+    print(f'Salary={sal} and Commission={commission}')
+else:
+    print(f'Salary={sal}')
+
+
+
