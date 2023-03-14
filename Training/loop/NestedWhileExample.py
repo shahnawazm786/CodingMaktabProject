b57e599b73ec5ebba5f2d0279c3a741780ea7a2b
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-14 23:10:12 +0530

    coding maktab

--- Training/loop/NestedWhileExample.py
+++ Training/loop/NestedWhileExample.py
@@ -20,16 +20,27 @@ x=1
 while x<=4:
     y=1
     while y<=4:
-        print(x,end='')
+        print(x,end=' ')
         y=y+1
     print()
     x=x+1
 print()
-print("Pattern -2 ")
+print("Patter -2")
+l=1
+while l<=4:
+    m=1
+    while m<=l:
+        print(m,end=' ')
+        m=m+1
+    print()
+    l=l+1
+print()
+print("Pattern -3 ")
 z=1
 while z<=4:
     a=1
     while a<=z:
-       print(z,end='')
+       print(z,end=' ')
        a=a+1
+    print()
     z=z+1
