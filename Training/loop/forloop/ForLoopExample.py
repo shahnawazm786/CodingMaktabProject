51a1c342f88494c322c9fb2986774aee75d0ead3
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-15 22:21:31 +0530

    coding maktab

--- Training/loop/forloop/ForLoopExample.py
+++ Training/loop/forloop/ForLoopExample.py
@@ -24,3 +24,5 @@ for i in range(1,5):
     for j in range(1,i+1):
         print(i,end=' ')
     print()
+
+
---
+++ Training/loop/forloop/printmethodtermination.py
@@ -0,0 +1,12 @@
+print("Hello")
+print("world")
+print("-----------------------")
+print("Hello",end="")
+print("world")
+
+#print() end="\n" by default
+print("hello","world",sep="        ")
+
+print("hello","world",sep="**************")
+print("home","users","python","programming",sep="/")
+print("python","seleniu","pytest","junit",sep=',')
