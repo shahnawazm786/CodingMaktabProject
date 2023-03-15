0c97a358c9bb6f5172606b731f1665b18d6c9ca2
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-15 22:13:49 +0530

    coding maktab

---
+++ Training/loop/forloop/ForLoopExample.py
@@ -0,0 +1,9 @@
+for x in range(1,11):
+    print(x)
+str="i am learning python programming"
+vowel_count=0
+for s in str:
+    if(s=='a' or s=='i' or s=='e' or s=='o' or s=='u'):
+        vowel_count=vowel_count+1
+
+print(f"Vowel count is ->{vowel_count}")
