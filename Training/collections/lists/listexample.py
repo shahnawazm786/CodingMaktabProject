9eb91fc84fd796cccf97090d5259a5f8b712b26a
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-15 22:36:31 +0530

    coding maktab

---
+++ Training/collections/lists/listexample.py
@@ -0,0 +1,17 @@
+a=[]
+print(type(a))
+b=list()
+print(type(b))
+a.append(100)
+a.append(200)
+print(a)
+c=['java','python','c#','ruby']
+print(c)
+c.append("java script")
+print(c)
+c.pop()
+print(c)
+c.remove('python')
+print(c)
+c[1]="c language"
+print(c)
