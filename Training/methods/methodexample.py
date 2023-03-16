4737e815520760ff558759ef72ea8e6089ebf71e
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-16 22:56:55 +0530

    coding maktab

---
+++ Training/methods/methodexample.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    return a+b
+
+def rev(str):
+    return str[::-1]
+
+print(rev('python'))
+print(rev([10,20,30]))
+print(rev(('A','B','C')))
+print(add(10,20))
+print(add(70,20))
+print(add(100,20))
+
