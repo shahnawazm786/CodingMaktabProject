f465e9fc19c89c696a7db2005d953c97065d7373
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-16 22:56:59 +0530

    coding maktab

---
+++ Training/collections/lists/miscexample2.py
@@ -0,0 +1,11 @@
+l=[10,20,30,('A','B'),{'name':'Python','page':50},40]
+print(l)
+#change the value of list at 0 index
+l[0]=5
+print(l)
+#l[3][0]='E' # touple
+l[3]=('Apple','Mango','Grapes')
+print(l)
+l[4]['name']='Java'
+print(l)
+
