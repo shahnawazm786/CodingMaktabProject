bde8599c5c3088e70505f8e196111a1b64b1d754
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-16 22:56:48 +0530

    coding maktab

---
+++ Training/collections/keywords/keywordsexample.py
@@ -0,0 +1,20 @@
+#break
+#continue
+#pass
+i=1
+while(i<=10):
+    if(i>5):
+        break
+    print(i)
+    i=i+1
+print('------------ continue --------')
+i=0
+while(i<10):
+    i = i + 1
+    if(i<6):
+        continue
+    print(i)
+
+j=20
+while j<=20:
+    pass
