4a648a7b6331b200c1a591f84c37419d3ba3ef20
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-19 15:22:56 +0530

    coding maktab

--- Training/methods/anonymousmethod.py
+++ Training/methods/anonymousmethod.py
@@ -41,4 +41,10 @@ numbers = (1, 2, 3, 4)
 result = map(addition, numbers)
 print(list(result))
 
+# Double all numbers using map and lambda
+
+numbers = (1, 2, 3, 4)
+result = map(lambda x: x + x, numbers)
+print(list(result))
+
 
