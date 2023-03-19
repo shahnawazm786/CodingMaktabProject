5c7655f9faf9d5bed7b67ab7ea4de262c312d93a
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-19 15:23:33 +0530

    coding maktab

--- Training/methods/anonymousmethod.py
+++ Training/methods/anonymousmethod.py
@@ -47,4 +47,10 @@ numbers = (1, 2, 3, 4)
 result = map(lambda x: x + x, numbers)
 print(list(result))
 
+numbers1 = [1, 2, 3]
+numbers2 = [4, 5, 6]
+
+result = map(lambda x, y: x + y, numbers1, numbers2)
+print(list(result))
+
 
