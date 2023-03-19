a5f554340ec5f0f1a3903e5889f28cdd29f65ed1
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-19 15:30:24 +0530

    coding maktab

--- Training/methods/anonymousmethod.py
+++ Training/methods/anonymousmethod.py
@@ -102,5 +102,20 @@ print(functools.reduce(operator.mul, lis))
 
 # using reduce to concatenate string
 print("The concatenated product is : ", end="")
-print(functools.reduce(operator.add, ["geeks", "for", "geeks"]))
+print(functools.reduce(operator.add, ["python", "for", "students"]))
+
+'''
+reduce() vs accumulate() 
+------------------------
+Both reduce() and accumulate() can be used to calculate the summation of a sequence elements. 
+But there are differences in the implementation aspects in both of these.  
+
+reduce() is defined in “functools” module, accumulate() in “itertools” module.
+reduce() stores the intermediate result and only returns the final summation value. 
+Whereas, accumulate() returns a iterator containing the intermediate results. 
+The last number of the iterator returned is summation value of the list.
+reduce(fun, seq) takes function as 1st and sequence as 2nd argument. 
+In contrast accumulate(seq, fun) takes sequence as 1st argument and function as 2nd argument.
+'''
+
 
