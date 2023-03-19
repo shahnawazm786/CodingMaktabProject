af1e69dad6d9b7447631639505d71a0ca8b42703
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-19 15:33:30 +0530

    coding maktab

--- Training/methods/anonymousmethod.py
+++ Training/methods/anonymousmethod.py
@@ -167,3 +167,14 @@ filtered = filter(fun, sequence)
 print('The filtered letters are:')
 for s in filtered:
     print(s)
+
+# a list contains both even and odd numbers.
+seq = [0, 1, 2, 3, 5, 8, 13]
+
+# result contains odd numbers of the list
+result = filter(lambda x: x % 2 != 0, seq)
+print(list(result))
+
+# result contains even numbers of the list
+result = filter(lambda x: x % 2 == 0, seq)
+print(list(result))
