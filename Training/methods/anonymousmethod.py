c3962460b0ea42a6018867e5d794f427f6c67614
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-19 15:29:07 +0530

    coding maktab

--- Training/methods/anonymousmethod.py
+++ Training/methods/anonymousmethod.py
@@ -81,3 +81,26 @@ print(functools.reduce(lambda a, b: a + b, lis))
 print("The maximum element of the list is : ", end="")
 print(functools.reduce(lambda a, b: a if a > b else b, lis))
 
+# importing functools for reduce()
+import functools
+
+# importing operator for operator functions
+import operator
+
+# initializing list
+lis = [1, 3, 5, 6, 2]
+
+# using reduce to compute sum of list
+# using operator functions
+print("The sum of the list elements is : ", end="")
+print(functools.reduce(operator.add, lis))
+
+# using reduce to compute product
+# using operator functions
+print("The product of list elements is : ", end="")
+print(functools.reduce(operator.mul, lis))
+
+# using reduce to concatenate string
+print("The concatenated product is : ", end="")
+print(functools.reduce(operator.add, ["geeks", "for", "geeks"]))
+
