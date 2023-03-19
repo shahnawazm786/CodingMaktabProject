82863bcf0c25b8fe91da1330e3e2b131da44e001
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-19 15:28:12 +0530

    coding maktab

--- Training/methods/anonymousmethod.py
+++ Training/methods/anonymousmethod.py
@@ -53,4 +53,19 @@ numbers2 = [4, 5, 6]
 result = map(lambda x, y: x + y, numbers1, numbers2)
 print(list(result))
 
+'''
+The reduce(fun,seq) function is used to apply a particular function passed in 
+its argument to all of the list elements mentioned in the sequence passed along.This function is 
+defined in “functools” module.
+
+Working :  
+
+At first step, first two elements of sequence are picked and the result is obtained.
+Next step is to apply the same function to the previously attained result and the number just 
+succeeding the second element and the result is again stored.
+This process continues till no more elements are left in the container.
+The final returned result is returned and printed on console.
+
+'''
+
 
