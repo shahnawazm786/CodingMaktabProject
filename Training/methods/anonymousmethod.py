e4581cae5fcbb077d3630d2f48faa33201debe30
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-19 15:33:06 +0530

    coding maktab

--- Training/methods/anonymousmethod.py
+++ Training/methods/anonymousmethod.py
@@ -133,3 +133,37 @@ print(list(itertools.accumulate(lis, lambda x, y: x + y)))
 print("The summation of list using reduce is :", end="")
 print(functools.reduce(lambda x, y: x + y, lis))
 
+'''
+The filter() method filters the given sequence with the help of a function that tests 
+each element in the sequence to be true or not. 
+syntax:
+filter(function, sequence)
+Parameters:
+function: function that tests if each element of a 
+sequence true or not.
+sequence: sequence which needs to be filtered, it can 
+be sets, lists, tuples, or containers of any iterators.
+Returns:
+returns an iterator that is already filtered.
+
+'''
+
+
+# function that filters vowels
+def fun(variable):
+    letters = ['a', 'e', 'i', 'o', 'u']
+    if (variable in letters):
+        return True
+    else:
+        return False
+
+
+# sequence
+sequence = ['g', 'e', 'e', 'j', 'k', 's', 'p', 'r']
+
+# using filter function
+filtered = filter(fun, sequence)
+
+print('The filtered letters are:')
+for s in filtered:
+    print(s)
