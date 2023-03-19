4199040f7cd3a12c0010af2447e6d2ceeff8749a
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-19 15:22:11 +0530

    coding maktab

--- Training/methods/anonymousmethod.py
+++ Training/methods/anonymousmethod.py
@@ -31,5 +31,14 @@ for pl, sc in zip(players, scores):
 map() function returns a map object(which is an iterator) of the 
 results after applying the given function to each item of a given iterable (list, tuple etc.)
 '''
+# map(fun, iter)
+def addition(n):
+    return n + n
+
+
+# We double all numbers using map()
+numbers = (1, 2, 3, 4)
+result = map(addition, numbers)
+print(list(result))
 
 
