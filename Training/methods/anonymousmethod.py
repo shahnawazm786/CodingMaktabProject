844a89860b080d0280f06fd370f4c263074c040f
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-19 15:28:35 +0530

    coding maktab

--- Training/methods/anonymousmethod.py
+++ Training/methods/anonymousmethod.py
@@ -68,4 +68,16 @@ The final returned result is returned and printed on console.
 
 '''
 
+import functools
+
+# initializing list
+lis = [1, 3, 5, 6, 2]
+
+# using reduce to compute sum of list
+print("The sum of the list elements is : ", end="")
+print(functools.reduce(lambda a, b: a + b, lis))
+
+# using reduce to compute maximum element from list
+print("The maximum element of the list is : ", end="")
+print(functools.reduce(lambda a, b: a if a > b else b, lis))
 
