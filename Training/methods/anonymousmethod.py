24ed3ac2c697490e7857f32fc226ff39a8c15eb4
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-19 15:30:45 +0530

    coding maktab

--- Training/methods/anonymousmethod.py
+++ Training/methods/anonymousmethod.py
@@ -117,5 +117,19 @@ The last number of the iterator returned is summation value of the list.
 reduce(fun, seq) takes function as 1st and sequence as 2nd argument. 
 In contrast accumulate(seq, fun) takes sequence as 1st argument and function as 2nd argument.
 '''
+import itertools
 
+# importing functools for reduce()
+import functools
+
+# initializing list
+lis = [1, 3, 4, 10, 4]
+
+# printing summation using accumulate()
+print("The summation of list using accumulate is :", end="")
+print(list(itertools.accumulate(lis, lambda x, y: x + y)))
+
+# printing summation using reduce()
+print("The summation of list using reduce is :", end="")
+print(functools.reduce(lambda x, y: x + y, lis))
 
