3b35003390d4013071917911f88b3aa244ba04e1
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-20 22:50:09 +0530

    coding maktab

--- Training/methods/filtermethod.py
+++ Training/methods/filtermethod.py
@@ -1,6 +1,12 @@
-def filter(val):
-    l=[]
-    for x in val:
-        if(x%3==0):
-            l.append(x)
-    return l
+def filt(val):
+        if(val%3==0):
+            return val
+
+print(filt(4))
+num=[2,3,4,5,6,7,8,9]
+res1= filter(filt,num)
+print(res1)
+print(list(res1))
+res2=filter(lambda x : x%3==0,num)
+print(res2)
+print(list(res2))
