4872c8cf5e01f6d2f0959f79dc75bdf7e2424d9a
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-20 22:20:15 +0530

    coding maktab

--- Training/methods/anonymousmethod1.py
+++ Training/methods/anonymousmethod1.py
@@ -15,3 +15,16 @@ print(cub)
 print(type(cub))
 print(cub(4))
 
+def greater(val1,val2):
+    if val1>val2:
+        return val1
+    else:
+        return  val2
+
+print(greater(100,300))
+print(greater(500,300))
+
+grt=lambda x,y : x if x>y else y
+print(grt(300,5))
+
+
