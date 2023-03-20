9e6d3f51ff016db0efe80299a1afbb1c5fd7bffb
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-20 22:15:09 +0530

    coding maktab

---
+++ Training/methods/anonymousmethod1.py
@@ -0,0 +1,17 @@
+def sum(para):
+    return para+10
+print(sum(10))
+
+prt=lambda :"Hello!!! World"
+print(prt)
+print(prt())
+
+def cube(x):
+    return x**3
+
+print(cube(3))
+cub=lambda para: para**3
+print(cub)
+print(type(cub))
+print(cub(4))
+
