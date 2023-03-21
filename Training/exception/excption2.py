75771d8220e6168ac67131747488c46b839926dc
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-21 23:22:50 +0530

    coding maktab

--- Training/exception/excption2.py
+++ Training/exception/excption2.py
@@ -1,1 +1,23 @@
 x=input('Enter any value')
+y=input('Enter another value')
+tot=0
+try:
+    print(x+y)
+except(TypeError):
+    print('Excpetion raised')
+    tot=int(x)+int(y)
+    print(tot)
+finally:
+    del(x)
+    del(y)
+    print('deleted the x and y variable')
+
+try:
+    print(x)
+except(NameError):
+    print('x is not visible')
+try:
+    print(y)
+except:
+    print('y is already deleted')
+
