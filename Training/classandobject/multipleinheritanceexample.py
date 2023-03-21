753a9192cdbabc2acc115134a68f869f90c4a6e6
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-21 22:34:29 +0530

    coding maktab

--- Training/classandobject/multipleinheritanceexample.py
+++ Training/classandobject/multipleinheritanceexample.py
@@ -3,18 +3,28 @@ class Animal:
         print('Animal class')
     def who_am_i(self):
         print('I am from Animal')
+    def tanga(self):
+        print('Tanga')
 
 class Car:
     def __int__(self):
         print('Car Class')
     def who_am_i(self):
         print('I am from Car')
+    def wheel(self):
+        print('I have wheel')
 
 class Baghi(Animal,Car):
-    def __int__(self):
+    def __init__(self):
         print('Baghi Class')
     def who_am_i(self):
         print('I am from Baghi')
+
+b=Baghi()
+b.who_am_i()
+b.wheel()
+b.tanga()
+
 
 
 
