2e9e451920d2ba41f52ea134a6693795cd6993e2
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-03-21 22:43:33 +0530

    coding maktab

--- Training/classandobject/multilevelinheritance.py
+++ Training/classandobject/multilevelinheritance.py
@@ -9,11 +9,23 @@ class Link(Pen):
         print('I am link class')
     def write(self):
         print('Writing smooth')
+    def height(self):
+        print('height is normal')
 
 class Reynolds(Link):
     def __init__(self):
         print('I am Reynolds')
     def write(self):
         print('Writing crispy')
+    def color(self):
+        print('bottom is white and up is blue')
 
+r=Reynolds()
+r.color()
+r.height()
+r.write()
+l=Link()
+l.write()
+p=Pen()
+p.write()
 
