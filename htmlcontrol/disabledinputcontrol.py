1617a84f34e355b358405058c92b46e485109a21
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-01 12:37:33 +0530

    coding maktab

--- htmlcontrol/disabledinputcontrol.py
+++ htmlcontrol/disabledinputcontrol.py
@@ -2,16 +2,18 @@ from selenium import webdriver
 import time
 
 
-from selenium.common import NoSuchElementException
+from selenium.common import NoSuchElementException, ElementNotInteractableException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 driver=webdriver.Chrome()
 driver.implicitly_wait(5)
 driver.get('https://www.selenium.dev/selenium/web/web-form.html')
 time.sleep(5)
-passw=driver.find_element(By.XPATH,value="//textarea[@name='my-textarea']")
-passw.send_keys('Hello',Keys.ENTER)
-passw.send_keys('Hello1',Keys.ENTER)
-passw.send_keys('Hello2',Keys.ENTER)
+passw=driver.find_element(By.XPATH,value="//input[@name='my-disabled']")
+try:
+    passw.send_keys('Hello')
+except(ElementNotInteractableException):
+    print('we can not add text in the disabled input box')
+    print(passw.text)
 time.sleep(5)
 driver.quit()
