281d27dca95971738e445a246279878e4417e029
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-01 18:03:16 +0530

    coding maktab

---
+++ htmlcontrol/screenshot.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import time
+
+from selenium.webdriver.common.by import By
+
+driver=webdriver.Chrome()
+driver.implicitly_wait(5)
+driver.get('https://www.selenium.dev/selenium/web/web-form.html')
+time.sleep(5)
+radio=driver.find_elements(By.XPATH,value="//input[@name='my-radio']")
+for r in radio:
+    if(r.is_selected()):
+        r.click()
+    else:
+        r.click()
+
+# take the screenshot
+driver.get_screenshot_as_file("/screenshot/fullpage.png")
+driver.close()
