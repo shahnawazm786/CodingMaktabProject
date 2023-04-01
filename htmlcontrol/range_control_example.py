6666e5efca850161c8c5345d742895c966a6d360
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-01 15:10:39 +0530

    coding maktab

--- htmlcontrol/range_control_example.py
+++ htmlcontrol/range_control_example.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 import time
 
-
 from selenium.common import NoSuchElementException, ElementNotInteractableException
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.color import Color
@@ -12,6 +12,9 @@ driver=webdriver.Chrome()
 driver.implicitly_wait(5)
 driver.get('https://www.selenium.dev/selenium/web/web-form.html')
 time.sleep(5)
-element=driver.find_element(By.XPATH,value="//input[@name='my-date']")
-element.click()
+element=driver.find_element(By.XPATH,value="//input[@name='my-range']")
+action=ActionChains(driver)
+action.drag_and_drop_by_offset(element,20,100).perform()
+#action.release(element).perform()
 time.sleep(5)
+driver.quit()
