8a8ae5e21bd4564500c1c6a6cdb2c1cf7e202262
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-01 13:06:22 +0530

    coding maktab

--- htmlcontrol/check_box_control.py
+++ htmlcontrol/check_box_control.py
@@ -12,3 +12,12 @@ driver=webdriver.Chrome()
 driver.implicitly_wait(5)
 driver.get('https://www.selenium.dev/selenium/web/web-form.html')
 time.sleep(5)
+check=driver.find_elements(By.XPATH,value="//input[@name='my-check']")
+for chk in check:
+    if(chk.is_selected()):
+        chk.click()
+    else:
+        chk.click()
+
+time.sleep(5)
+driver.quit()
