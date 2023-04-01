ce23d43a4b4146edfd790ef23f4751c12a9ea219
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-01 12:46:07 +0530

    coding maktab

--- htmlcontrol/dropdowncontrol.py
+++ htmlcontrol/dropdowncontrol.py
@@ -1,3 +1,4 @@
+
 from selenium import webdriver
 import time
 
@@ -5,8 +6,15 @@ import time
 from selenium.common import NoSuchElementException, ElementNotInteractableException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+
 driver=webdriver.Chrome()
 driver.implicitly_wait(5)
 driver.get('https://www.selenium.dev/selenium/web/web-form.html')
 time.sleep(5)
-readonly=driver.find_element(By.XPATH,value="//input[@name='my-readonly']")
+select_element = driver.find_element(By.NAME, 'my-select')
+select = Select(select_element)
+select.select_by_value(1)
+select.select_by_visible_text("Two")
+time.sleep(5)
+driver.quit()
