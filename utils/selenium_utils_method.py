ead4aa65b539ba4c4eb87e2598486af8167d356d
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-01 15:33:47 +0530

    coding maktab

---
+++ utils/selenium_utils_method.py
@@ -0,0 +1,8 @@
+class selenium_common_function:
+    @classmethod
+    def waitTillLoading(self, element,duration):
+        pass
+
+    @classmethod
+    def waitElementClickable(self,element,duration):
+        pass
