380bd5675def681e0b66abb47db655254a8d8123
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-01 15:24:07 +0530

    coding maktab

--- htmlcontrol/color_picker_control.py
+++ htmlcontrol/color_picker_control.py
@@ -3,6 +3,7 @@ import time
 
 
 from selenium.common import NoSuchElementException, ElementNotInteractableException
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.color import Color
@@ -14,11 +15,16 @@ driver.get('https://www.selenium.dev/selenium/web/web-form.html')
 time.sleep(5)
 colors=driver.find_element(By.XPATH,value="//input[@name='my-colors']")
 colors.click()
+##563d7c
+actions=ActionChains(driver)
+actions.move_to_element_with_offset(colors,0,100).perform()
+'''
 for i in range(100):
     colors.send_keys(Keys.ARROW_RIGHT)
     colors.click()
 #colors.send_keys(Keys.ARROW_RIGHT)
 #colors.send_keys(Keys.ARROW_RIGHT)
 colors.click()
+'''
 time.sleep(15)
 driver.close()
