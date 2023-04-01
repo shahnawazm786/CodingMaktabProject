3ae394a76923fb1905449808ad83baf6b624a600
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-01 17:56:18 +0530

    coding maktab

--- utils/selenium_utils_method.py
+++ utils/selenium_utils_method.py
@@ -1,8 +1,17 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+
 class selenium_common_function:
-    @classmethod
-    def waitTillLoading(self, element,duration):
-        pass
 
     @classmethod
-    def waitElementClickable(self,element,duration):
-        pass
+    def waitTillLoading(cls,driver,element,duration):
+        return WebDriverWait(driver=driver,duration=duration,poll_frequency=2).until(
+            expected_conditions.visibility_of(element))
+
+    @classmethod
+    def waitTillElementClickable(cls,driver,val,duration):
+        return WebDriverWait(driver,duration).until(
+                expected_conditions.element_to_be_clickable(By.XPATH,value=val))
+
