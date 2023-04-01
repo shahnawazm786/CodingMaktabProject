58d2b62658237dd2bafd59acf2fac1c8b572aa73
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-01 12:41:25 +0530

    coding maktab

---
+++ htmlcontrol/readonlyinputcontrol.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import time
+
+
+from selenium.common import NoSuchElementException, ElementNotInteractableException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+driver=webdriver.Chrome()
+driver.implicitly_wait(5)
+driver.get('https://www.selenium.dev/selenium/web/web-form.html')
+time.sleep(5)
+readonly=driver.find_element(By.XPATH,value="//input[@name='my-readonly']")
+readonly.send_keys("hello!!!!")
+readonly.click()
+'''
+try:
+    passw.send_keys('Hello')
+except(ElementNotInteractableException):
+    print('we can not add text in the disabled input box')
+    print(passw.text)
+'''
+time.sleep(5)
+driver.quit()
