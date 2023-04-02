9da6d2058b734840f364d6d62b04be003c4a6398
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-02 12:34:40 +0530

    coding maktab - python automation test

--- iframe_switch/switch_iframe_example.py
+++ iframe_switch/switch_iframe_example.py
@@ -5,3 +5,7 @@ from selenium.webdriver.common.by import By
 driver=webdriver.Chrome()
 driver.implicitly_wait(5)
 driver.get('https://www.jqueryscript.net/tags.php?/event%20calendar/')
+frame=driver.find_element(By.ID,value="aswift_1")
+driver.switch_to.frame(frame)
+time.sleep(5)
+driver.quit()
