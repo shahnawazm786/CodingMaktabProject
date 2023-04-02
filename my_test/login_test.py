3c3f4b8acb2651925d37f3455f1a1f54f4e7b86e
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-02 12:59:34 +0530

    coding maktab - python automation test

---
+++ my_test/login_test.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+import time
+import pytest
+def test_login_successful():
+
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(5)
+    driver.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    user_name=driver.find_element(By.NAME,value="username")
+    password=driver.find_element(By.NAME,value="password")
+    btn=driver.find_element(By.TAG_NAME,value="button")
+    user_name.send_keys("Admin")
+    password.send_keys("admin123")
+    btn.click()
+    assert driver.title=="OrangeHRM"
+
+def test_not_login_successful():
+
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(5)
+    driver.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    user_name=driver.find_element(By.NAME,value="username")
+    password=driver.find_element(By.NAME,value="password")
+    btn=driver.find_element(By.TAG_NAME,value="button")
+    user_name.send_keys("Admin")
+    password.send_keys("")
+    btn.click()
+    assert driver.title==" "
