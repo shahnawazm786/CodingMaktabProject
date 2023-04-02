98ec396991b69eada2e1ee7864c55278f26ca1ea
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-02 11:28:27 +0530

    coding maktab - python automation test

--- exceptions/not_interactable_exception_selenium.py
+++ exceptions/not_interactable_exception_selenium.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 
-from selenium.common import NoSuchElementException, ElementNotInteractableException
+from selenium.common import NoSuchElementException, ElementNotInteractableException, InvalidElementStateException
 from selenium.webdriver.common.by import By
 driver=webdriver.Chrome()
 driver.implicitly_wait(5)
@@ -16,4 +16,10 @@ except(ElementNotInteractableException):
     ele.screenshot("./disable.png")
     driver.save_screenshot("./disabled_element_page.png")
 time.sleep(5)
+ele1=driver.find_element(By.NAME,"my-readonly")
+#ele1.send_keys("Hello")
+try:
+    ele1.clear()
+except(InvalidElementStateException):
+    print('We can not clear the read only text box')
 driver.close()
