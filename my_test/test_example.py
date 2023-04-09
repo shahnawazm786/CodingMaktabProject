51b14e4f76222c3906edf44e117e153ea8e2967e
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-09 10:22:51 +0530

    coding maktab - python automation test

---
+++ my_test/test_example.py
@@ -0,0 +1,22 @@
+import pytest
+
+def test_first():
+    assert True
+
+def test_second():
+    assert False
+
+
+'''
+def test_third(num):
+    assert num>10
+def test_four(num1,num2):
+    cal=num1+num2
+    assert cal<500
+'''
+
+def test_abc():
+    print('Hello!!!')
+
+def test_hello_print():
+    print('hello!!!!')
---
+++ my_test/test_example_01.py
@@ -0,0 +1,10 @@
+def test_square_cal_for_three():
+    assert 3*3 == 9
+
+def test_square_cal_for_four():
+    assert 4*4 == 16
+
+def test_square_cal_for_five():
+    assert 5*5 == 25
+
+
