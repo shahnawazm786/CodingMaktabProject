8178055585f8d9d7c7bfabf0de6b0f1c31f043b5
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-09 16:02:19 +0530

    coding maktab - python automation test

---
+++ my_test/test_fail_example.py
@@ -0,0 +1,60 @@
+import pytest
+
+def test_one():
+    print("fail")
+    assert False
+
+def test_two():
+    print("pass")
+    assert True
+
+def test_three():
+    print("pass")
+    assert True
+def test_four():
+    print("pass")
+    assert True
+def test_fve():
+    print("pass")
+    assert True
+def test_six():
+    print("pass")
+    assert True
+
+def test_seven():
+    print("pass")
+    assert True
+
+def test_eight():
+    print("fail")
+    assert False
+
+def test_nine():
+    print("fail")
+    assert False
+
+def test_ten():
+    print("fail")
+    assert False
+
+def test_eleven():
+    print("fail")
+    assert True
+
+def test_eleven1():
+    print("fail")
+    assert True
+
+def test_eleven2():
+    print("fail")
+    assert True
+
+def test_eleven3():
+    print("fail")
+    assert True
+
+
+
+
+
+
