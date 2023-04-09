13b6c9ffbe8e09c45f79619fa4908c1eb953a15c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-09 11:19:15 +0530

    coding maktab - python automation test

--- my_test/test_fixture_example.py
+++ my_test/test_fixture_example.py
@@ -1,1 +1,21 @@
 import pytest
+@pytest.fixture
+def test_fixture_one():
+    val=51
+    return val
+def test_fixture_two(test_fixture_one):
+    a=test_fixture_one/2
+    print(a)
+    assert a == 25.5
+
+def test_fixture_three(test_fixture_one):
+    assert test_fixture_one/3 == 0
+
+def test_fixture_four(test_fixture_one):
+    assert test_fixture_one/4 == 3
+
+def test_fixture_five(test_fixture_one):
+    assert test_fixture_one/5 == 1
+
+
+
