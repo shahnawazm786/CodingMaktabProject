74f8cf0e49a241b7f994069a10bbd578c4e02456
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-12 10:11:43 +0530

    coding maktab - python automation test

--- numpy/arrays/arrays_example.py
+++ numpy/arrays/arrays_example.py
@@ -1,3 +1,10 @@
 import numpy as np
+
+#array created from list
 arr=np.array([1,2,3,4,5])
+print(type(arr))
 print(arr)
+# array created by tuple
+arr1=np.array((1,2,3,4,5))
+print(arr1)
+print(type(arr1))
