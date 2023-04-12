feb7d9823fcf5fb6d876e709cce28f996a06714e
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-12 10:12:43 +0530

    coding maktab - python automation test

--- numpy/arrays/arrays_example.py
+++ numpy/arrays/arrays_example.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 #array created from list
-#Create a 2-D array containing two arrays with the values 1,2,3 and 4,5,6:
+#Create a 1-D array containing two arrays with the values 1,2,3 and 4,5,6:
 arr=np.array([1,2,3,4,5])
 print(type(arr))
 print(arr)
