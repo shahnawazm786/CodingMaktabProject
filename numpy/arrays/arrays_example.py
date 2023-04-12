0d3f113deb761a4d5d12e4cf3eb8075219c30832
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-04-12 10:12:32 +0530

    coding maktab - python automation test

--- numpy/arrays/arrays_example.py
+++ numpy/arrays/arrays_example.py
@@ -1,10 +1,12 @@
 import numpy as np
 
 #array created from list
+#
 arr=np.array([1,2,3,4,5])
 print(type(arr))
 print(arr)
 # array created by tuple
+#Create a 1-D array containing two arrays with the values 1,2,3 and 4,5,6:
 arr1=np.array((1,2,3,4,5))
 print(arr1)
 print(type(arr1))
