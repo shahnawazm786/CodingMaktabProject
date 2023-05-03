92047b0026eec1586b4fa68a46ddb48ac6df7eed
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-05-03 16:19:22 +0530

    pom - python

--- orangehrms_page_object_model/test/Test_Login.py
+++ orangehrms_page_object_model/test/Test_Login.py
@@ -1,7 +1,8 @@
+from selenium import webdriver
 class Test001_Login:
     baseURL='https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
     userName='Admin'
     password='admin123'
 
-
-
+    def test_home_page_title(self):
+        pass
