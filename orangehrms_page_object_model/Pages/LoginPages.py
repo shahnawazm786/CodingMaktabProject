f96a4c50723a8b3251d3a2ba4a2b37de3f3611df
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-05-03 17:06:45 +0530

    page object model - pytest

--- orangehrms_page_object_model/Pages/LoginPages.py
+++ orangehrms_page_object_model/Pages/LoginPages.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 class LoginPage:
-    inputUserTextBox_Name='By.NAME'
+    user_name_input_box_by_name='username'
     inputPasswordTextBox_By_Name='password'
     btnLogin_By_Xpath="//button[@type='submit']"
     imgLogo_By_Xpath="(//img)[1]"
@@ -10,9 +10,9 @@ class LoginPage:
         self.driver=driver
 
     def enterUserName(self,username):
-        print(username)
-        self.driver.find_element(By.NAME,self.inputUserTextBox_Name).clear()
-        self.driver.find_element(By.NAME,self.inputUserTextBox_Name).send_keys(username)
+
+        self.driver.find_element(By.NAME,self.user_name_input_box_by_name).clear()
+        self.driver.find_element(By.NAME,self.user_name_input_box_by_name).send_keys(username)
 
     def enterPassword(self,password):
         print(password)
